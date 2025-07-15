f50bc01862e6c4e5ec1a0a54ea9e383158f2248a
tonywabuko <tonywabuko@gmail.com>
2025-07-15 12:55:52 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,13 @@
 # run.py
+from dotenv import load_dotenv
+import os
+
+load_dotenv()  # This loads variables from your .env file
+
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
